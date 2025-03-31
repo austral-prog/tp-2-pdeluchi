017e0a41f45be142ddd2a4986f9fbb61281c3dcc
pdeluchi <pdeluchi@mail.austral.edu.ar>
2025-03-31 14:39:54 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    fullname= f"{first_name} {last_name}"
+
+    print (fullname.lower() )
+    print (fullname.title())
+    print (fullname.upper())
+    print(f"\t{fullname.lower()}")
