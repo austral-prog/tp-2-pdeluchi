b23e0b9318f50acbc48b0f4b2f392427bd1a8f04
pdeluchi <pdeluchi@mail.austral.edu.ar>
2025-03-31 14:39:24 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,7 +1,9 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
-    print (fullname.lower() )
-    print (fullname.title())
-    print (fullname.upper())
-    print(f"\t{fullname.lower()}")
+    res1= x<y
+    res2 = y<x
+
+    print (f"The result of {x} comes first in the dictionary than {y} is {res1}.")
+
+    print (f"The result of {y} comes first in the dictionary than {x} is {res2}.")
